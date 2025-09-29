eb74a6723174cae0c7b44f7ded86810501dde346
My Little Jellyfish <unsigned.angler.19937@proton.me>
2025-09-29 15:27:28 +0300

    refactor(experiment): minor
    - standard include guards
    - sort includes
    - const auto

--- Lab1Variant8/Experiment.h
+++ Lab1Variant8/Experiment.h
@@ -1,16 +1,18 @@
-#pragma once
+#ifndef EXPERIMENT_H
+#define EXPERIMENT_H
 
 #include <string>
 #include <utility>
 
 // Class for managing experiments and parameter sweeps
-class Experiment 
-{
-public:
-    static void runSingle(int n, int k, int trials, unsigned int seed);
-    
-    static void runSweep(int n, int trials, unsigned int seed, 
-                         int k_max, int step, const std::string& output_file);
-    
-    static void printUsage(const char* prog);
+class Experiment {
+ public:
+  static void runSingle(int n, int k, int trials, unsigned int seed);
+
+  static void runSweep(int n, int trials, unsigned int seed, int k_max,
+                       int step, const std::string& output_file);
+
+  static void printUsage(const char* prog);
 };
+
+#endif  // EXPERIMENT_H
