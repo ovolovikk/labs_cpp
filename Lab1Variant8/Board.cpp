982ca4b020159110ccb2cd2a0d8bcf2c039c75a9
My Little Jellyfish <unsigned.angler.19937@proton.me>
2025-09-29 15:26:05 +0300

    refactor(board): modernize cpp standard
    - emplace
    - contains
    - const auto

--- Lab1Variant8/Board.cpp
+++ Lab1Variant8/Board.cpp
@@ -1,62 +1,59 @@
-
 #include "Board.h"
+
 #include <stdexcept>
 
 Board::Board(int n) : size(n) {
-    if (n <= 0) {
-        throw std::invalid_argument("Board size must be positive");
-    }
-    reset();
+  if (n <= 0) {
+    throw std::invalid_argument("Board size must be positive");
+  }
+  reset();
 }
 
 void Board::addSelectedCell(int row, int col) {
-    if (!isInBounds(row, col)) {
-        throw std::out_of_range("Cell coordinates out of board range");
-    }
-    selected_cells.insert({ row, col });
+  if (!isInBounds(row, col)) {
+    throw std::out_of_range("Cell coordinates out of board range");
+  }
+  selected_cells.emplace(row, col);
 }
 
-void Board::reset() {
-    selected_cells.clear();
-}
+void Board::reset() { selected_cells.clear(); }
 
 bool Board::isInBounds(int row, int col) const {
-    return row >= 0 && row < size && col >= 0 && col < size;
+  return row >= 0 && row < size && col >= 0 && col < size;
 }
 
 int Board::calculateFreeZoneSize() const {
-    int free_zone = 0;
-    for (int r = 0; r < size; ++r) {
-        for (int c = 0; c < size; ++c) {
-
-            if (!isAdjacentToSelected(r, c)) {
-                free_zone++;
-            }
-        }
+  int free_zone = 0;
+  for (int r = 0; r < size; ++r) {
+    for (int c = 0; c < size; ++c) {
+      if (!isAdjacentToSelected(r, c)) {
+        free_zone++;
+      }
     }
-    return free_zone;
+  }
+  return free_zone;
 }
 
 bool Board::isAdjacentToSelected(int row, int col) const {
-    const int dr[] = {-1, 0, 0, 1};  // up, left, right, down
-    const int dc[] = {0, -1, 1, 0};  // up, left, right, down
-    
-    // First check if the cell itself is selected
-    if (selected_cells.count({row, col}) > 0) {
+  const int dr[] = {-1, 0, 0, 1};  // up, left, right, down
+  const int dc[] = {0, -1, 1, 0};  // up, left, right, down
+
+  // TODO(@ovolovikk): use contains from C++20
+  if (selected_cells.contains({row, col})) {
+    return true;
+  }
+
+  for (int i = 0; i < 4; i++) {
+    // TODO(@ovolovikk): use const auto
+    const auto new_row = row + dr[i];
+    const auto new_col = col + dc[i];
+
+    if (isInBounds(new_row, new_col)) {
+      if (selected_cells.contains({new_row, new_col})) {
         return true;
+      }
     }
-    
-    // Then check adjacent cells
-    for (int i = 0; i < 4; i++) {
-        int new_row = row + dr[i];
-        int new_col = col + dc[i];
-        
-        if (isInBounds(new_row, new_col)) {
-            if (selected_cells.count({new_row, new_col}) > 0) {
-                return true;
-            }
-        }
-    }
-    
-    return false;
+  }
+
+  return false;
 }
